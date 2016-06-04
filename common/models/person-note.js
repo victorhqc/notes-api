21e30f4318fbe4265099038df0ecf2f4841f7f86
Victor Quiroz <developer@Admins-MacBook-Pro.local>
2016-06-03 21:27:57 -0500

    Note Timestamps In order to store the Notes, an operation hook was used 'before save' so it sets the timestamps correctly

--- common/models/person-note.js
+++ common/models/person-note.js
@@ -1,3 +1,14 @@
 module.exports = function(PersonNote) {
+    PersonNote.observe('before save', function addTimeStamps(ctx, next) {
 
+        let now = new Date();
+        if(ctx.instance) {
+            ctx.instance.updatedAt = now;
+            if( ctx.isNewInstance ) {
+                ctx.instance.createdAt = now;
+            }
+        }
+
+        next();
+    });
 };
